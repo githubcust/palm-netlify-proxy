ca1d16eeb04a19a86c426e0ca18a031d72f27556
githubcust <44195234+githubcust@users.noreply.github.com>
2025-01-08 14:32:02 +0800

    Update proxy.ts

--- netlify/functions/proxy.ts
+++ netlify/functions/proxy.ts
@@ -57,7 +57,7 @@ export default async (request: Request, context: Context) => {
     url.searchParams.append(key, value);
   });
 
-  const headers = pickHeaders(request.request.headers, ["content-type", "accept-encoding"]);
+  const headers = pickHeaders(request.headers, ["content-type", "accept-encoding"]);
 
   const response = await fetch(url, {
     body: request.body,
